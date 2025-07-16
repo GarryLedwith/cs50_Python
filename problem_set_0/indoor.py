8d53dc995553a50c7c04ba35364bc633f68ac2a0
Garry Ledwith <garryledwith@gmail.com>
2025-07-16 06:44:11 +0100

    answering first two questions

---
+++ problem_set_0/indoor.py
@@ -0,0 +1,4 @@
+# prompt user for input 
+# output the same input in lowercase
+user_input = input("Enter some text: ") # prompt for user input
+print(user_input.lower())  # print the input in lowercaseG
---
+++ problem_set_0/playback.py
@@ -0,0 +1,6 @@
+# prompt user for input 
+# output the same input
+# replacing each space with '...'
+
+user_input = input("Enter some text: ")  # prompt for user input
+print(user_input.replace(" ", "..."))  # print the input with spaces replaced by '...'
