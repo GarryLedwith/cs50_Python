c52800d6f9019fcdf22687c847179fcf70d1b93a
Garry Ledwith <garryledwith@gmail.com>
2025-07-28 11:01:32 +0100

    looping through dicts and converting strings to floats

---
+++ Week_2/distances.py
@@ -0,0 +1,21 @@
+distances = {
+    "Voyager 1": "163.7",
+    "Voyager 2": "134.7",
+    "Pioneer 10": "82.5",
+    "Pioneer 11": "70.6",
+    "New Horizons": "6.5"
+}
+
+
+def main():
+    for name in distances.keys():  # use the keys() method to iterate over the dictionary
+        # print(f"{name}: {distances[name]} AU from Earth")
+        print(f"{name}: {convert_au_to_meters(float(distances[name]))} meters from Earth")
+
+# convert au to meters
+def convert_au_to_meters(au):
+    return au * 149597870700  # 1 AU in meters
+
+      
+main()
+    
