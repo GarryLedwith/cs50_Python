fd18401aaf3c1bc0efe7e59912e72725b8adefdd
Garry Ledwith <garryledwith@gmail.com>
2025-07-28 10:52:51 +0100

    working on dicts

--- Week_2/report.py
+++ Week_2/report.py
@@ -1,6 +1,7 @@
 def main(): 
     spacecraft = {"name": "James Webb Space Telescope"}
-    spacecraft["distance"] = "0.1" # set distance
+    spacecraft.update({"distance": "0.1"})  # set distance using update method
+    # spacecraft["distance"] = "0.1"  # set distance using direct assignment
     print(create_report(spacecraft))
     
 
@@ -9,7 +10,7 @@ def create_report(spacecraft):
     ====== Report ========
     
     Name: {spacecraft['name']}
-    Distance: {spacecraft['distance']} AU
+    Distance: {spacecraft.get("distance", "Unknown")} AU 
     
     ======================
     """ 
