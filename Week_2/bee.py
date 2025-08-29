9c4553edac7667e1e74f4d6fc81f4ef3a8988b98
Garry Ledwith <garryledwith@gmail.com>
2025-08-29 05:59:20 +0100

    working on week 2

---
+++ Week_2/bee.py
@@ -0,0 +1,15 @@
+WORDS = {"PAIR": 4, "HAIR": 4, "CHAIR": 5}
+
+def main():
+    print("Welcome to the spelling bee game!")
+    print("Your letters are: A, I, P, C, R, H, G")
+    
+    while len(WORDS) > 0: 
+        print(f"{len(WORDS)} words left to guess.")
+        guess = input("Guess a word: ").upper()
+        
+        # TODO: Check if the guess is valid
+        
+    print("That's the game!")
+    
+main()
--- Week_2/distances.py
+++ Week_2/distances.py
@@ -11,6 +11,9 @@ def main():
     for name in distances.keys():  # use the keys() method to iterate over the dictionary
         # print(f"{name}: {distances[name]} AU from Earth")
         print(f"{name}: {convert_au_to_meters(float(distances[name]))} meters from Earth")
+        
+    for distance in distances.values():
+        print(f"{distance} AU is {convert_au_to_meters(float(distance))} meters")
 
 # convert au to meters
 def convert_au_to_meters(au):
